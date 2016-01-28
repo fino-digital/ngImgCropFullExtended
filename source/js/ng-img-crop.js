54922e14bf299872afc41b19911b1040fb6607a4
David Kronenberger <david.kronenberger@fino.digital>
2016-01-28 08:40:04 +0100

    Revert "Check if updateResultImage results in slow drag on mobile. TEST"
    This reverts commit f2a5a567f50b1deb4411e1bbb2c35642c2ca6cae.

--- source/js/ng-img-crop.js
+++ source/js/ng-img-crop.js
@@ -117,16 +117,15 @@ crop.directive('imgCrop', ['$timeout', 'cropHost', 'cropPubSub', function($timeo
                         updateResultImage(scope);
                     }
                 }))
-                .on('image-updated', fnSafeApply(function(scope) {
-                    updateResultImage(scope);
-                }))
                 .on('area-move-end area-resize-end', fnSafeApply(function (scope) {
                     // if (!scope.tada) {
                     //   scope.tada = true;
-                    updateResultImage(scope);
+                      updateResultImage(scope);
                     // }
+                }))
+                .on('image-updated', fnSafeApply(function(scope) {
+                    updateResultImage(scope);
                 }));
-
 
             // Sync CropHost with Directive's options
             scope.$watch('image', function(newVal) {
